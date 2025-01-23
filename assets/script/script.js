0873bfe0ff89cee93cebd5249894b830996acd6d
Little-Lighthouse <139311825+Little-Lighthouse@users.noreply.github.com>
2025-01-23 19:48:35 +0630

    V2.2.0
    - Changes to Navbar
    - Improvements in pageSection
    - Minor updates

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,18 @@
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  })
+})
+
+hiddenElements.forEach((el) => observer.observe(el));
+
 /* ---- NOT AVAILABLE ---- */
 function notAvaSer(){
     alert("The requested profile is currently not available.\nWe apologize for the inconvenience.");

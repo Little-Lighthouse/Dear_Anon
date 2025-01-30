1d881c7cda5a2e1e03d4d72e6173f824bbe6561e
Little-Lighthouse <139311825+Little-Lighthouse@users.noreply.github.com>
2025-01-30 16:55:03 +0630

    V2.2.5
    - Added text indent
    - Added new pages
    - Minor changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,17 @@
-const hiddenElements = document.querySelectorAll(".hidden");
+/* ---- TRANSITION ---- */
+const hiddenElements = document.querySelectorAll(".hiddenTransit");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
     if(entry.isIntersecting){
-      entry.target.classList.add("show");
+      entry.target.classList.add("showTransit");
     }
   })
 })
 
 hiddenElements.forEach((el) => observer.observe(el));
 
+
 /* ---- NOT AVAILABLE ---- */
-function notAvaSer(){
-    alert("The requested profile is currently not available.");
-  }
+function notAvaSer(){alert("The requested profile is currently not available.");}
